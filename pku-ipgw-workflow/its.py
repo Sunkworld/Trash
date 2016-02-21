a0adccfec27045d05c4202604dae2f83b58859c3
sunkworld <sunkworld@yandex.com>
2016-02-21 19:23:34 +0800

    Revert "revision"
    This reverts commit 9a5ef14b22bea422c0d6609b6f1e0556c87c99a4.

--- pku-ipgw-workflow/its.py
+++ pku-ipgw-workflow/its.py
@@ -1,15 +1,36 @@
-#!/usr/bin/python
 # coding:utf-8
-import urllib
-query = "{query}"
+import urllib,urllib2
+query = '{query}'
 
-with open('/Applications/setting.txt') as f:
-    usrn = f.readline().strip()
-    pswd = f.readline().strip()
+def connect_free():
+    value1 = {'cmd':'open', 'type':'free', 'fr':'0', 'sid':'873'}
+    res = opener.open('https://its.pku.edu.cn/netportal/PKUIPGW', urllib.urlencode(value1))
+
+def connect_fee():
+    value2 = {'cmd':'open', 'type':'fee', 'fr':'0', 'sid':'855'}
+    res = opener.open('https://its.pku.edu.cn/netportal/PKUIPGW', urllib.urlencode(value2))
+
+def disconnect_all():
+    value3 = {'cmd':'close', 'type':'allconn', 'fr':'0', 'sid':'734'}
+    opener.open('https://its.pku.edu.cn/netportal/PKUIPGW', urllib.urlencode(value3))
+
+
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
+
+f = open('/Applications/setting.txt')
+usrn = f.readline().replace('\n','')
+pswd = f.readline().replace('\n','')
+f.close()
+str = '|;kiDrqvfi7d$v0p5Fg72Vwbv2;|'
+url = 'https://its.pku.edu.cn/cas/login'
+opener.open(url)
+value = {'username1':usrn, 'password':pswd, 'pwd_t':'密码', 'fwrd':'noopen', 'username':usrn+str+pswd+str+'15'}
+opener.open(url, urllib.urlencode(value))
 
 if query == '1':
-    urllib.urlopen('https://its.pku.edu.cn:5428/ipgatewayofpku?uid='+usrn+'&password='+pswd+'&range=2&operation=connect&timeout=1')
+    connect_free()
 elif query == '2':
-    urllib.urlopen('https://its.pku.edu.cn:5428/ipgatewayofpku?uid='+usrn+'&password='+pswd+'&range=1&operation=connect&timeout=1')
+    connect_fee()
 elif query == '3':
-    urllib.urlopen('https://its.pku.edu.cn:5428/ipgatewayofpku?uid='+usrn+'&password='+pswd+'&range=2&operation=disconnectall&timeout=1')
+    disconnect_all()
+quit()
