2b9c8c5927cae5a53163778508cf8c197e495214
sunkworld <sunkworld@yandex.com>
2016-03-12 13:10:14 +0800

    modification

--- auto-check-weibo/weibo.py
+++ auto-check-weibo/weibo.py
@@ -13,13 +13,13 @@ reload(sys)
 sys.setdefaultencoding( "utf-8" )
 
 class autochkweibo:
+    relaxtime = RELAX_TIME
     
     def __init__(self, usrid):
         self.usrid = usrid
         self.url = 'http://weibo.cn/' + usrid
         self.fanurl = 'http://weibo.cn/' + usrid +'/fans'
         self.followurl = 'http://weibo.cn/' + usrid + '/follow'
-        self.relaxtime = RELAX_TIME
         self.headers = {'User-Agent':'Chrome'}
         with open(os.path.expanduser('~/.weibocookie')) as f:
             self.headers['Cookie'] = f.read().strip()
@@ -68,15 +68,16 @@ class autochkweibo:
                 self.weibolist = []
                 for item in self.soup.find_all('div','c', id=True):
                     self.weibolist.append(item['id'])
-                tmp = BeautifulSoup(requests.get(self.fanurl, headers=self.headers).text)
-                self.fanslist = []
-                for item in tmp.find_all('td', valign='top', style=None):
-                    self.fanslist.append(item.a.string)
-                tmp = BeautifulSoup(requests.get(self.followurl, headers=self.headers).text)
-                self.followlist = []
-                for item in tmp.find_all('td', valign='top', style=None):
-                    self.followlist.append(item.a.string)
-                print "User Info Fetched: %s" % self.name
+                self.fanslist = self.getList(self.fanurl)
+                self.followlist = self.getList(self.followurl)
+                print "User Info Fetched: %s\n" % self.name
+                print "简介：%s\n" % self.profile
+                print "关注人列表："
+                for item in self.followlist:
+                    print item
+                print "\n粉丝列表："
+                for item in self.fanslist:
+                    print item
                 break
             except Exception, e:
                 self.sm(self.soup_text.encode('utf-8'), "Error in Function %s" % sys._getframe().f_code.co_name)
@@ -131,22 +132,15 @@ class autochkweibo:
         tip2 = self.soup.find('div','tip2')
         tmp = re.findall(r'\[(.*?)\]', tip2.get_text().encode('utf-8'))
         followcount = int(tmp[1])
-        while True:
-            try:
-                tmp = BeautifulSoup(requests.get(self.followurl, headers=self.headers).text)
-                break
-            except Exception, e:
-                sleep(100)
-        if re.search('自动跳转', tmp.encode('utf-8')):
-            self.sm(tmp.prettify().encode('utf-8'), "Error in Function %s" % sys._getframe().f_code.co_name)
+        followlist = self.getList(self.followurl)
+        if not followlist:
             return False
-        followlist = []
-        for item in tmp.find_all('td', valign='top', style=None):
-            followlist.append(item.a.string)
         newfollow = []
         for item in followlist:
             if item not in self.followlist:
                 newfollow.append(item)
+            else:
+                break
         if len(newfollow):
             msg = "%s当前关注了%s位用户。\n\n新关注用户有：\n" % (self.name, followcount)
             for item in newfollow:
@@ -156,37 +150,67 @@ class autochkweibo:
                 msg += "%s\n" % item
             sub = "%s有新关注的用户" % self.name
             self.sm(msg, sub)
+        elif followcount < self.followcount:
+            msg = "%s当前关注了%s位用户。\n" % (self.name, followcount)
+            msg += "\n目前关注用户列表为：\n"
+            for item in followlist:
+                msg += "%s\n" % item
+            sub = "%s取关了%s个用户" % (self.name, str(self.followcount - followcount))
+            self.sm(msg, sub)
         self.followcount = followcount
         self.followlist = followlist
         
+    def getList(self, myurl):
+        for ii in range(3):
+            try:
+                tmp = BeautifulSoup(requests.get(myurl, headers=self.headers).text)
+                c = int(re.search('1/(.*?)页', tmp.prettify().encode('utf-8')).group(1))
+                mylist = []
+                for item in tmp.find_all('td', valign='top', style=None):
+                    mylist.append(item.a.string)
+                for i in range(2, min(4, c+1)):
+                    tmp = BeautifulSoup(requests.get(myurl+'?page='+str(i), headers=self.headers).text)
+                    #        print tmp
+                    for item in tmp.find_all('td', valign='top', style=None):
+                        mylist.append(item.a.string)
 
-
+                return mylist
+            except Exception, e:
+                print e
+                time.sleep(100)
+        self.sm(tmp.prettify().encode('utf-8'), "Error in Function %s" % sys._getframe().f_code.co_name)
+        return False
+                
     def chk_fans(self):
         tip2 = self.soup.find('div','tip2')
         tmp = re.findall(r'\[(.*?)\]', tip2.get_text().encode('utf-8'))
         fanscount = int(tmp[2])
-        while True:
-            try:
-                tmp = BeautifulSoup(requests.get(self.fanurl, headers=self.headers).text)
-                break
-            except Exception, e:
-                sleep(100)
-        if re.search('自动跳转', tmp.encode('utf-8')):
-            self.sm(tmp.prettify().encode('utf-8'), "Error in Function %s" % sys._getframe().f_code.co_name)
+        fanslist = self.getList(self.fanurl)
+        if not fanslist:
             return False
-        fanslist = []
-        for item in tmp.find_all('td', valign='top', style=None):
-            fanslist.append(item.a.string)
         newfan = []
         for item in fanslist:
             if item not in self.fanslist:
                 newfan.append(item)
+            else:
+                break
         if len(newfan):
             msg = "%s当前有%s位粉丝。\n\n新增粉丝有：\n" % (self.name, fanscount)
             for item in newfan:
                 msg += "%s\n" % item
+            msg += "\n目前粉丝列表为：\n"
+            for item in fanslist:
+                msg += "%s\n" % item
             sub = "有新用户关注了%s" % self.name
             self.sm(msg, sub)
+        elif fanscount < self.fanscount:
+            msg = "%s当前有%s位粉丝。\n" % (self.name, fanscount)
+            msg += "\n目前粉丝列表为：\n"
+            for item in fanslist:
+                msg += "%s\n" % item
+            sub = "%s移除了%s个粉丝" % (self.name, str(self.fanscount - fanscount))
+            self.sm(msg, sub)
+
         self.fanscount = fanscount
         self.fanslist = fanslist
     
@@ -214,7 +238,10 @@ class autochkweibo:
             if self.getContent():
                 break
             else:
-                sleep(500)
+                time.sleep(500)
+        if re.search('自动跳转', self.soup.prettify().encode('utf-8')):
+            self.sm(tmp.prettify().encode('utf-8'), "Error in Function %s" % sys._getframe().f_code.co_name)
+            return False
         if CHK_WEIBO:
             self.chk_weibo(CHK_WEIBO_DELETED)
         if CHK_FOLLOW:
@@ -225,25 +252,28 @@ class autochkweibo:
             self.chk_profile()
         if AUTO_LIKE:
             self.auto_like()        
+        
+
+        
 
 if __name__ == '__main__':
 #    s = raw_input("请输入TA的微博id：")
-#    s = '1197191492'
     s = DESTINATED_WEIBO
     p = []
     for item in s:
         p.append(autochkweibo(item))
         p[len(p)-1].getInfo()
         time.sleep(2)
-    while True:
-        try:
+    try:
+        while True:
             for item in p:
                 item.chk()
                 time.sleep(5)
-        except KeyboardInterrupt:
-            print "\nScript is terminated."
-        time.sleep(item.relaxtime)
+        time.sleep(autochkweibo.relaxtime)
 
+    except KeyboardInterrupt:
+        print "\nScript is terminated."
+        
 
 
 
